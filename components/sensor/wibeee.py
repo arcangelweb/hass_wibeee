c061672a34c3bdd298b9fdcf9bbae8b63c7c2f98
juanjoSanz <juanjo.sanzmartin@gmail.com>
2019-01-18 01:42:35 +0100

    Update wibeee.py

--- components/sensor/wibeee.py
+++ components/sensor/wibeee.py
@@ -10,7 +10,7 @@ from datetime import timedelta
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_SCAN_INTERVAL)
+from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_SCAN_INTERVAL, CONF_PHASES)
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
